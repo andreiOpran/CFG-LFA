ff0c121e83637a5cc3ce1249d7d7e0f8b6b7ba19
Andrei Opran <andrei.opran@icloud.com>
2024-05-15 00:44:48 +0300

    loop infinit

--- LCFG LFA.cpp
+++ LCFG LFA.cpp
@@ -3,6 +3,22 @@
 #include <random>
 #include <string>
 using namespace std;
+
+int gasireCratimaN(vector <string> vector, int n) // functie care gaseste al n lea "-" dintr-un vector
+{
+	
+	int cont = 0;
+	for(int i = 0; i < vector.size(); i++)
+	{
+		if (vector[i] == "-")
+			cont++;
+		if(cont == n)
+			return i;
+	}
+	return -1;
+}
+
+
 int main()
 {
 	string numeFisier = "cfgInput.txt";
@@ -51,84 +67,66 @@ int main()
 	uniform_int_distribution<> dis(1, nrReguli);
 	reguli.pop_back();
 
-	int random1, random2, contorReguli1, contorReguli2, nrVariante;
-
 	vector <string> rez;
 	rez.push_back(start);
 
-	bool aplicatRegula;
-	
-	bool ok = false;
-	while (ok == false)
+	bool rezEsteVar, gasitRegula;
+	int random, index;
+
+	for (int i = 0; i < rez.size(); i++)
 	{
-		bool gasitVariabila = false;
-		for (int i = 0; i < rez.size(); i++)
+		rezEsteVar = false;
+		for (int j = 0; j < variabile.size(); j++)
 		{
-			for (int j = 0; j < variabile.size(); j++)
+			if (rez[i] == variabile[j])
 			{
-				if (rez[i] == variabile[j])
-				{
-					gasitVariabila = true;
-					break;
-				}
+				rezEsteVar = true;
+				break;
 			}
 		}
-		if (gasitVariabila == false)
-			ok = true;
 
-		if (ok == false)
+		if (rezEsteVar)
 		{
-			
-
-			for (int i = 0; i < rez.size(); i++)
+			gasitRegula = false;
+			while (gasitRegula == false)
 			{
-				for (int j = 0; j < variabile.size(); j++)
-				{
-				
-					cout << endl << endl;
-					for (int x = 0; x < rez.size(); x++)
-						cout << rez[x];
-					cout << endl << endl;
+				random = dis(gen);
+				index = gasireCratimaN(reguli, random);
+				if (reguli[index - 1] == rez[i])
+					gasitRegula = true;
+			}
+			if (index != -1)
+			{
+				index++;
 
-					aplicatRegula = false;
-					contorReguli1 = 0;
-					if (rez[i] == variabile[j])
+				while (index < reguli.size())
+				{
+					if(reguli[index] == "-")
+						break;
+					else
 					{
-						while (aplicatRegula == false)
-						{
-							random1 = dis(gen);
-							for (int k = 1; k < reguli.size(); k++)
-							{
-								if (reguli[k] == "-")
-									contorReguli1++;
-								if (contorReguli1 == random1 && reguli[k - 1] == rez[i])
-								{
-									int l = k + 1;
-									while (reguli[l] != "-" && l < reguli.size())
-									{
-										rez.insert(rez.begin() + i + 1, reguli[l]);
-										l++;
-										aplicatRegula = true;
-										if (l == reguli.size() || reguli[l] == "-")
-										{
-											aplicatRegula = true;
-											ok = true;
-											break;
-										}
-									}
-								}
-							}
-						}
+						rez.insert(rez.begin() + i, reguli[index]);
+						index++;
 					}
+					
 				}
 			}
+			
+		}
+		else
+		{
+			i = rez.size() + 2;
 		}
 		
+		
 	}
-	
+
 	for (int i = 0; i < rez.size(); i++)
 		cout << rez[i];
+;
 
 	return 0;
 }
 
+// REGULI 
+// A - 0 A 1 A - 0 1
